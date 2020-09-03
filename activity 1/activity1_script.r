79aa7d2764bdf4476d0929fe02331f8f9a47f7bc
cbarb21 <cbarbato@hamilton.edu>
2020-09-03 12:57:22 -0400

    performing 3 operations

--- activity 1/activity1_script.r
+++ activity 1/activity1_script.r
@@ -1,1 +1,11 @@
 print ("hello R world")
+#multiply 5 times 4
+5*4
+#name my number
+a <- 20
+#divide my number by 5
+a/5
+#make a vector of numbers
+b <- c(12,8,24,100)
+#multiply all numbers by 3
+b*3
